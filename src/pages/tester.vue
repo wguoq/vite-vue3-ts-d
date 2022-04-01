0f0c037b9b56d96c273eb4cf20f9a3cb4f003317
wguoq <wgq1218@gmail.com>
2022-04-01 18:34:52 +0800

    不用watch，改成CurrentChange

--- src/pages/tester.vue
+++ src/pages/tester.vue
@@ -1,11 +1,10 @@
 <script lang="ts" setup>
 import SingleTable from 'components/SingleTable.vue';
 import EditForm from 'components/EditForm.vue';
-import { ref,reactive,watch } from 'vue';
-import { ElMessage } from 'element-plus';
+import { ref,reactive,} from 'vue';
+import { ElMessage, ElTable } from 'element-plus';
 import { axiosSend, loading } from 'utils/http.ts'
 import TesterApi from 'api/tester.ts'
-// import {SingleTableProps} from 'components/PropsClass.ts'
 
 class TableProps{
 	api:any = null
@@ -43,10 +42,34 @@ const TestCaseTable = ref()
 const TcApiDataTable = ref()
 const TcCheckTable = ref()
 
-let TcApiTableRow = ref(null)
-let TestCaseTableRow = ref(null)
-let TcApiDataTableRow = ref(null)
-let TcCheckTableRow = ref(null)
+const TcApiTableCurrentChange=(currentRow:any,oldCurrentRow:any)=>{
+	if (TcApiTable.value?.current.row){
+		data.testCaseTableP.filters = {"tc_action_id": TcApiTable.value.current.row.id}
+	}else{
+		data.testCaseTableP.filters = null
+	}
+}
+
+const TestCaseTableCurrentChange=(currentRow:any,oldCurrentRow:any)=>{
+	if (TestCaseTable.value?.current.row){
+		data.tcApiDataTableP.filters = {"test_case": TestCaseTable.value.current.row.id}
+	}else{
+		data.tcApiDataTableP.filters = null
+	}
+}
+
+const TcApiDataTableCurrentChange=(currentRow:any,oldCurrentRow:any)=>{
+
+	if (TcApiDataTable.value?.current.row){
+		data.tcCheckTableP.filters = {"tc_data_id": TcApiDataTable.value.current.row.id}
+	}else{
+		data.tcCheckTableP.filters = null
+	}
+	
+}
+const TcCheckTableCurrentChange=(currentRow:any,oldCurrentRow:any)=>{
+
+}
 
 function init(){
 
@@ -76,60 +99,6 @@ function init(){
 
 }
 
-const updata=()=>{
-	//当数据发生改变后，重新获取每个列表的当前选中行
-	if (typeof(TcApiTable.value) != "undefined" && TcApiTable.value.current.row){
-		TcApiTableRow.value = TcApiTable.value.current.row
-	}else{
-		TcApiTableRow.value = null
-	}
-
-	if (typeof(TestCaseTable.value) != "undefined" && TestCaseTable.value.current.row){
-		TestCaseTableRow.value = TestCaseTable.value.current.row
-	}else{
-		TestCaseTableRow.value = null
-	}
-
-	if (typeof(TcApiDataTable.value) != "undefined" && TcApiDataTable.value.current.row){
-		TcApiDataTableRow.value = TcApiDataTable.value.current.row
-	}else{
-		TcApiDataTableRow.value = null
-	}
-
-	if (typeof(TcCheckTable.value) != "undefined" && TcCheckTable.value.current.row){
-		TcCheckTableRow.value = TcCheckTable.value.current.row
-	}else{
-		TcCheckTableRow.value = null
-	}
-}
-//监听每个列表选中行的变化来更新关联数据
-watch([TcApiTableRow,TestCaseTableRow,TcApiDataTableRow],(new_,old_) => {
-	if(new_[0] != old_[0]){
-		if (typeof(TcApiTable.value) != "undefined" && TcApiTable.value.current.row){
-			data.testCaseTableP.filters = {"tc_action_id": TcApiTable.value.current.row.id}
-		}else{
-			data.testCaseTableP.filters = null
-		}
-	}
-	
-	if(new_[1] != old_[1]){
-		if (typeof(TestCaseTable.value) != "undefined" && TestCaseTable.value.current.row){
-			data.tcApiDataTableP.filters = {"test_case": TestCaseTable.value.current.row.id}
-		}else{
-			data.tcApiDataTableP.filters = null
-		}
-	}
-
-	if(new_[2] != old_[2]){
-		if (typeof(TcApiDataTable.value) != "undefined" && TcApiDataTable.value.current.row){
-			data.tcCheckTableP.filters = {"tc_data_id": TcApiDataTable.value.current.row.id}
-		}else{
-			data.tcCheckTableP.filters = null
-		}
-	}
-
-})
-
 const addTcApi=()=>{
 	data.editForm = new FormProps()
 	data.editForm.action = "save"
@@ -141,9 +110,7 @@ const addTcApi=()=>{
 }
 
 const addTestCase=()=>{
-	if (TcApiTable.value.current.row == null || TcApiTable.value.current.row.id == null){
-		ElMessage.warning("没有选中数据")
-	}else{
+	if (TcApiTable.value?.current.row){
 		data.editForm = new FormProps()
 		data.editForm.action = "save"
 		data.editForm.api = TesterApi
@@ -152,6 +119,8 @@ const addTestCase=()=>{
 		data.editForm.hideLabel = ["id","code","created_time","modified_time"]
 		data.editForm.disabledLabel = ["version","tc_action_id"]
 		data.showDialog = true
+	}else{
+		ElMessage.warning("没有选中数据")
 	}
 }
 
@@ -171,9 +140,7 @@ const runTestCase=(row:any)=>{
 }
 
 const addTcApiData=()=>{	
-	if (TestCaseTable.value.current.row == null || TestCaseTable.value.current.row.id == null){
-		ElMessage.warning("没有选中数据")
-	}else{
+	if (TestCaseTable.value?.current.row){
 		data.editForm = new FormProps()
 		data.editForm.action = "save"
 		data.editForm.api = TesterApi
@@ -184,13 +151,15 @@ const addTcApiData=()=>{
 		data.editForm.hideLabel = ["id","code","created_time","modified_time"]
 		data.editForm.disabledLabel = ["version","test_case"]
 		data.showDialog = true
+		
+	}else{
+		ElMessage.warning("没有选中数据")
+
 	}
 }
 
 const addTcCheck=()=>{
-	if (TcApiDataTable.value.current.row == null || TcApiDataTable.value.current.row.id == null){
-		ElMessage.warning("没有选中数据")
-	}else{
+	if (TcApiDataTable.value?.current.row){
 		data.editForm = new FormProps()
 		data.editForm.action = "save"
 		data.editForm.api = TesterApi
@@ -201,32 +170,38 @@ const addTcCheck=()=>{
 		data.editForm.hideLabel = ["id","code","created_time","modified_time"]
 		data.editForm.disabledLabel = ["version","tc_data_id"]
 		data.showDialog = true
+		
+	}else{
+		ElMessage.warning("没有选中数据")
+
 	}
 }
 
-function reload(repo:string){
-	if(repo == 'TcApi'){
+function reload(name:string){
+	if(name == 'TcApi'){
 		data.tcApiTableP.filters = {}
 	}
-	else if(repo == 'TestCase'){
-		data.testCaseTableP.filters = {"tc_action_id": TcApiTable.value.current.row.id}
+	else if(name == 'TestCase'){
+		data.testCaseTableP.filters = {"tc_action_id": TcApiTable.value?.current.row.id}
 	}
-	else if(repo == 'TcApiData'){
-		data.tcApiDataTableP.filters = {"test_case": TestCaseTable.value.current.row.id}
+	else if(name == 'TcApiData'){
+		data.tcApiDataTableP.filters = {"test_case": TestCaseTable.value?.current.row.id}
 	}
-	else if(repo == 'TcCheckPoint'){
-		data.tcCheckTableP.filters = {"tc_data_id": TcApiDataTable.value.current.row.id}
+	else if(name == 'TcCheckPoint'){
+		data.tcCheckTableP.filters = {"tc_data_id": TcApiDataTable.value?.current.row.id}
 	}
 
 }
 
-const afterSave=(f:any)=>{
+const afterSave=(name:string)=>{
 	data.showDialog = false
-	reload(f.repo)
+	reload(name)
 }
 
 init()
-let noEditFields = ["id","code","created_time","modified_time","version","ver_status"]
+let noEditFields = ["id","code","created_time","modified_time","version","ver_status","test_case","tc_data_id"]
+
+
 </script>
 
 <template>
@@ -248,7 +223,7 @@ let noEditFields = ["id","code","created_time","modified_time","version","ver_st
 		:hideLabel= data.editForm.hideLabel
 		:readOnly = data.editForm.readOnly
 		:noSave = data.editForm.noSave
-		@afterSave = "afterSave"
+		@afterSave = "afterSave(data.editForm.repo)"
 		></EditForm>
 	</el-dialog>
 
@@ -270,8 +245,7 @@ let noEditFields = ["id","code","created_time","modified_time","version","ver_st
 		:fieldInfo=data.tcApiTableP.fieldInfo
 		:colwidth=data.tcApiTableP.colwidth
 		:noEditFields = noEditFields
-		@rowClick="updata"
-		@afterInit="updata"
+		@currentChange="TcApiTableCurrentChange"
 		>
 		</SingleTable>
 	</el-row>
@@ -294,8 +268,7 @@ let noEditFields = ["id","code","created_time","modified_time","version","ver_st
 		:fieldInfo=data.testCaseTableP.fieldInfo
 		:noEditFields = noEditFields
 		:colwidth=data.testCaseTableP.colwidth
-		@rowClick="updata"
-		@afterInit="updata"
+		@currentChange="TestCaseTableCurrentChange"
 		>
 			<template v-slot:SingleTableCol >
 			<el-table-column fixed="right" label="功能" width="70" >
@@ -333,8 +306,7 @@ let noEditFields = ["id","code","created_time","modified_time","version","ver_st
 		:fieldInfo=data.tcApiDataTableP.fieldInfo
 		:noEditFields = noEditFields
 		:colwidth=data.tcApiDataTableP.colwidth
-		@rowClick="updata"
-		@afterInit="updata"
+		@currentChange="TcApiDataTableCurrentChange"
 		>
 		</SingleTable>
 	</el-row>
@@ -357,8 +329,7 @@ let noEditFields = ["id","code","created_time","modified_time","version","ver_st
 		:fieldInfo=data.tcCheckTableP.fieldInfo
 		:noEditFields = noEditFields
 		:colwidth=data.tcCheckTableP.colwidth
-		@rowClick="updata"
-		@afterInit="updata"
+		@currentChange="TcCheckTableCurrentChange"
 		>
 		</SingleTable>
 	</el-row>
